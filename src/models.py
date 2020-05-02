17336290a7c8de98db6b33e5186bc6e07b8ca165
Jaehyeon Kim <dottami@gmail.com>
2020-05-02 01:03:51 +0000

    update database import

--- src/models.py
+++ src/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
 
-from .database import Base
+from src.database import Base
 
 
 class Employee(Base):
