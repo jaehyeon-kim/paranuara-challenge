3c38adcc70d1109d249996965e40e779932c47b1
Jaehyeon Kim <dottami@gmail.com>
2020-05-02 07:41:38 +0000

    remove hard-coded env vars

--- src/database.py
+++ src/database.py
@@ -4,11 +4,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-os.environ["POSTGRES_USER"] = "devuser"
-os.environ["POSTGRES_PASSWORD"] = "password"
-os.environ["POSTGRES_HOST"] = "localhost"
-os.environ["POSTGRES_DB"] = "devdb"
-
 SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}:5432/{}".format(
     os.environ["POSTGRES_USER"],
     os.environ["POSTGRES_PASSWORD"],
