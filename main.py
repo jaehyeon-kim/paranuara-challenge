78600f15d0b89864cf978d9f7238742d0b7565c7
Jaehyeon Kim <dottami@gmail.com>
2020-05-02 04:40:58 +0000

    update doc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, Query, Path
 from sqlalchemy.orm import Session
 
 from src import crud, models, schemas
@@ -17,7 +17,12 @@ def get_db():
         db.close()
 
 
-@app.get("/company/list/", response_model=schemas.Company, tags=["company"])
+@app.get(
+    "/company/list/",
+    response_model=schemas.Company,
+    tags=["company"],
+    summary="Provide a list of companies - quick way to get company details",
+)
 def get_companies(skip: int = 0, limit: int = 50, db: Session = Depends(get_db)):
     db_users = crud.get_companies(db, skip, limit)
     return db_users
@@ -26,10 +31,14 @@ def get_companies(skip: int = 0, limit: int = 50, db: Session = Depends(get_db))
 @app.get(
     "/company/{company_id}/employee",
     response_model=List[schemas.Employee],
-    tags=["company"],
     responses={404: {"model": schemas.BasicError}},
+    tags=["company"],
+    summary="Given a company, provide all the employees.",
 )
-def get_company_employees(company_id: int, db: Session = Depends(get_db)):
+def get_company_employees(
+    company_id: int = Path(..., ge=0, description="non negative integer"),
+    db: Session = Depends(get_db),
+):
     employees = crud.get_company_employees(db, company_id)
     if employees is None:
         raise HTTPException(404, detail="company not found")
@@ -41,11 +50,14 @@ def get_company_employees(company_id: int, db: Session = Depends(get_db)):
 @app.get(
     "/employee/relation/",
     response_model=schemas.EmployeeFriends,
-    tags=["employee"],
     responses={404: {"model": schemas.BasicError}},
+    tags=["employee"],
+    summary="Given 2 people, provide their information and the list of their friends in common which have brown eyes and are still alive.",
 )
 def get_employee_relation(
-    first_employee_id: int, second_employee_id: int, db: Session = Depends(get_db)
+    first_employee_id: int = Query(..., ge=0, description="non negative integer"),
+    second_employee_id: int = Query(..., ge=0, description="non negative integer"),
+    db: Session = Depends(get_db),
 ):
     employee_friends = crud.get_employee_relation(db, first_employee_id, second_employee_id)
     if not employee_friends:
@@ -56,10 +68,14 @@ def get_employee_relation(
 @app.get(
     "/employee/{employee_id}/favourite_food",
     response_model=schemas.EmployeeFood,
-    tags=["employee"],
     responses={404: {"model": schemas.BasicError}},
+    tags=["employee"],
+    summary="Given 1 people, provide a list of fruits and vegetables they like.",
 )
-def get_employee_food(employee_id: int, db: Session = Depends(get_db)):
+def get_employee_food(
+    employee_id: int = Path(..., ge=0, description="non negative integer"),
+    db: Session = Depends(get_db),
+):
     employee_food = crud.get_employee_food(db, employee_id)
     print(employee_food)
     if not employee_food:
