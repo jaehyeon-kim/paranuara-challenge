22a28196574a470190fb925385adf9b766dcec64
Jaehyeon Kim <dottami@gmail.com>
2020-05-02 07:42:05 +0000

    add test cases

---
+++ tests/test_main.py
@@ -0,0 +1,73 @@
+import pytest
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get_companies():
+    resp = client.get("/company/list", params={"skip": 0, "limit": 20})
+    assert resp.status_code == 200, resp.text
+
+    data = resp.json()
+    assert len(data["companies"]) == 20
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        {"company_id": -1, "status_code": 422, "note": "invalid id"},
+        {"company_id": 1, "status_code": 200, "note": "success"},
+        {"company_id": 0, "status_code": 404, "note": "no employee"},
+        {"company_id": 200, "status_code": 404, "note": "no company"},
+    ],
+)
+def test_get_company_employees(test_input):
+    resp = client.get(f"/company/{test_input['company_id']}/employee")
+    assert resp.status_code == test_input["status_code"], resp.text
+
+    if resp.status_code == 200:
+        data = resp.json()
+        assert all([d["company_id"] == test_input["company_id"] for d in data])
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        {
+            "first_employee_id": -1,
+            "second_employee_id": 2,
+            "status_code": 422,
+            "note": "invalid id",
+        },
+        {"first_employee_id": 1, "second_employee_id": 2, "status_code": 200, "note": "success"},
+        {
+            "first_employee_id": 2000,
+            "second_employee_id": 2,
+            "status_code": 404,
+            "note": "no employee",
+        },
+    ],
+)
+def test_get_employee_relation(test_input):
+    resp = client.get(
+        "/employee/relation/",
+        params={
+            "first_employee_id": test_input["first_employee_id"],
+            "second_employee_id": test_input["second_employee_id"],
+        },
+    )
+    assert resp.status_code == test_input["status_code"], resp.text
+
+
+@pytest.mark.parametrize(
+    "test_input",
+    [
+        {"employee_id": -1, "status_code": 422, "note": "invalid id"},
+        {"employee_id": 1, "status_code": 200, "note": "success"},
+        {"employee_id": 2000, "status_code": 404, "note": "no employee"},
+    ],
+)
+def test_get_employee_food(test_input):
+    resp = client.get(f"/employee/{test_input['employee_id']}/favourite_food")
+    assert resp.status_code == test_input["status_code"], resp.text
